de59a2cf46009d3f0acd8e3037547eeb6306a1b3
Amos Omondi <amos.omondi@andela.com>
2016-01-22 11:42:52 +0300

    Flesh out room represeantation as a dictionary

--- models/rooms.py
+++ models/rooms.py
@@ -1,18 +1,24 @@
 class Room(object):
-
+    """The superclass Room. All room types
+    will inherit from this class. The init
+    method initializes every room with a name"""
     def __init__(self, name):
         self.name = name
 
 
 class Office(Room):
     capacity = 6
+    type = "OFFICE"
+    spaces = ['0', '0', '0', '0', '0', '0']
 
-    def __init__(self, name):
-        pass
+    def __repr__(self):
+        return '{"name": \"%s\", "type" : \"%s\", "capacity" : %d, "spaces" : %s}' % (self.name, self.type, self.capacity, self.spaces)
 
 
 class LivingSpace(Room):
     capacity = 4
+    type = "LIVING"
+    spaces = ['0', '0', '0', '0']
 
-    def __init__(self, name):
-        pass
+    def __repr__(self):
+        return '{"name": \"%s\", "type" : \"%s\", "capacity" : %d, "spaces" : %s}' % (self.name, self.type, self.capacity, self.spaces)
