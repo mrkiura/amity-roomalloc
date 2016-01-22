40b7efe6ff05dafddc57edecb6f937d570daead1
Amos Omondi <amos.omondi@andela.com>
2016-01-22 16:24:26 +0300

    Create ten offices and ten living spaces

---
+++ populate.py
@@ -0,0 +1,27 @@
+from models.rooms import Office, LivingSpace
+
+# Initializes ten office spaces, from O1
+# to O10
+O1 = Office("Office1")
+O2 = Office("Office2")
+O3 = Office("Office3")
+O4 = Office("Office4")
+O5 = Office("Office5")
+O6 = Office("Office6")
+O7 = Office("Office7")
+O8 = Office("Office8")
+O9 = Office("Office9")
+O10 = Office("Office10")
+
+# Initializes ten living spaces, from L1
+# to L10
+L1 = LivingSpace("LivingSpace1")
+L2 = LivingSpace("LivingSpace2")
+L3 = LivingSpace("LivingSpace3")
+L4 = LivingSpace("LivingSpace4")
+L5 = LivingSpace("LivingSpace5")
+L6 = LivingSpace("LivingSpace6")
+L7 = LivingSpace("LivingSpace7")
+L8 = LivingSpace("LivingSpace8")
+L9 = LivingSpace("LivingSpace9")
+L10 = LivingSpace("LivingSpace10")
