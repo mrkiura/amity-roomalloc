8db1f5e7b1edb46f574b1289b36966358944e627
Amos Omondi <amos.omondi@andela.com>
2016-01-22 10:46:28 +0300

    Add model class for people

---
+++ models/people.py
@@ -0,0 +1,12 @@
+class Person(object):
+    def __init__(self, name, position):
+        self.name = name
+        self.position = position
+
+
+class Fellow(Person):
+    pass
+
+
+class Staff(Person):
+    pass
