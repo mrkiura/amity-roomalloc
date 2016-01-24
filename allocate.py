6b5b67951794c62a55cd709073cd2eabf2e39089
Amos Omondi <amos.omondi@andela.com>
2016-01-24 22:40:26 +0300

    Add templates for basic functions to be coded later

---
+++ allocate.py
@@ -0,0 +1,40 @@
+import sys
+
+print sys.argv[1]
+
+# allocate function
+# will be called if sys.argv[1] is allocate and
+# sys.argv[2] is a txt file
+
+
+def allocate():
+    pass
+
+
+# get allocations function
+# will be called if sys.argv[1] is get and
+# sys.argv[2] is allocations
+# shows the rooms and the people assigned
+# to them onscreen
+
+def get_allocations():
+    pass
+
+
+# print allocations function
+# output allocations to a txt file
+# will be called if sys.argv[1] is print
+# and sys.argv[2] is allocations
+
+def print_allocations():
+    pass
+
+# will be given a room name and
+# print the members allocated to it
+# will be called if sys.argv[1] is print
+# and sys.argv[2] is members
+# and sys.argv[3] is a room name
+
+
+def print_members():
+    pass
