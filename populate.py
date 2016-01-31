1aa308c6bdc9b4f5ba0e4f2886a22f8d988b7f6e
Amos Omondi <amos.omondi@andela.com>
2016-01-31 11:32:47 +0300

    Refactor code: Move all code into populate function

--- populate.py
+++ populate.py
@@ -1,70 +1,69 @@
 import sqlite3
 from models.rooms import Office, LivingSpace
 
-# Initializes ten office spaces, from O1
-# to O10
-O1 = Office("Office1")
-O2 = Office("Office2")
-O3 = Office("Office3")
-O4 = Office("Office4")
-O5 = Office("Office5")
-O6 = Office("Office6")
-O7 = Office("Office7")
-O8 = Office("Office8")
-O9 = Office("Office9")
-O10 = Office("Office10")
 
-# Initializes ten living spaces, from L1
-# to L10
-L1 = LivingSpace("LivingSpace1")
-L2 = LivingSpace("LivingSpace2")
-L3 = LivingSpace("LivingSpace3")
-L4 = LivingSpace("LivingSpace4")
-L5 = LivingSpace("LivingSpace5")
-L6 = LivingSpace("LivingSpace6")
-L7 = LivingSpace("LivingSpace7")
-L8 = LivingSpace("LivingSpace8")
-L9 = LivingSpace("LivingSpace9")
-L10 = LivingSpace("LivingSpace10")
+def populate():
+    # lists of the room names
+    office_names = ["Narnia", "Camelot", "Krypton", "Valhalla", "Hogwarts",
+                    "Mordor", "Occulus", "Midgard", "Orange", "Turqoise"]
 
+    livingspace_names = ["Wood", "Paper", "Stone", "Wick", "Tower",
+                         "Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw",
+                         "Terra"]
 
-"""Populate database with empty rooms"""
-conn = sqlite3.connect("data/roomalloc.db")
+    # will hold the rooms objects created from the models
+    office_objects = []
+    livingspace_objects = []
 
-cursor = conn.cursor()
+    # initialize office spaces
+    for i in range(0, 10):
+        office_objects.append(Office(office_names[i]))
 
-cursor.execute("""CREATE TABLE IF NOT EXISTS rooms
-                  (name text PRIMARY KEY, type text, capacity integer, spaces text)""")
+    # initialize living spaces
+    for i in range(0, 10):
+        livingspace_objects.append(LivingSpace(livingspace_names[i]))
 
-offices = [(O1.name, O1.type, O1.capacity, ",".join(O1.spaces)),
-           (O2.name, O2.type, O2.capacity, ",".join(O2.spaces)),
-           (O3.name, O3.type, O3.capacity, ",".join(O3.spaces)),
-           (O4.name, O4.type, O4.capacity, ",".join(O4.spaces)),
-           (O5.name, O5.type, O5.capacity, ",".join(O5.spaces)),
-           (O6.name, O6.type, O6.capacity, ",".join(O6.spaces)),
-           (O7.name, O7.type, O7.capacity, ",".join(O7.spaces)),
-           (O8.name, O8.type, O8.capacity, ",".join(O8.spaces)),
-           (O9.name, O9.type, O9.capacity, ",".join(O9.spaces)),
-           (O10.name, O10.type, O10.capacity, ",".join(O10.spaces))]
+    # Populate database with empty rooms
+    conn = sqlite3.connect("data/roomalloc.db")
+    cursor = conn.cursor()
 
-livings = [(L1.name, L1.type, L1.capacity, ",".join(L1.spaces)),
-           (L2.name, L2.type, L2.capacity, ",".join(L2.spaces)),
-           (L3.name, L3.type, L3.capacity, ",".join(L3.spaces)),
-           (L4.name, L4.type, L4.capacity, ",".join(L4.spaces)),
-           (L5.name, L5.type, L5.capacity, ",".join(L5.spaces)),
-           (L6.name, L6.type, L6.capacity, ",".join(L6.spaces)),
-           (L7.name, L7.type, L7.capacity, ",".join(L7.spaces)),
-           (L8.name, L8.type, L8.capacity, ",".join(L8.spaces)),
-           (L9.name, L9.type, L9.capacity, ",".join(L9.spaces)),
-           (L10.name, L10.type, L10.capacity, ",".join(L10.spaces))]
+    # will hold tuples containing information for
+    # each room to be written to the database
+    office_populator = []
+    livingspace_populator = []
 
-try:
-    cursor.executemany("INSERT INTO rooms VALUES (?, ?, ?, ?)", offices)
-    conn.commit()
+    for i in range(0, 10):
+        office_populator.append((office_objects[i].name,
+                                 office_objects[i].type,
+                                 office_objects[i].capacity,
+                                ",".join(office_objects[i].spaces)))
 
-    cursor.executemany("INSERT INTO rooms VALUES (?, ?, ?, ?)", livings)
-    conn.commit()
+    for i in range(0, 10):
+        livingspace_populator.append((livingspace_objects[i].name,
+                                     livingspace_objects[i].type,
+                                     livingspace_objects[i].capacity,
+                                     ",".join(livingspace_objects[i].spaces)))
 
-    print "Database populated successfully"
-except sqlite3.IntegrityError:
-    print "Already populated"
+    # create rooms table
+    cursor.execute("""CREATE TABLE IF NOT EXISTS rooms
+                   (name text PRIMARY KEY, type text, capacity integer, spaces text)""")
+
+    # insert the data (empty rooms)
+    try:
+        #  insert offices
+        cursor.executemany("INSERT INTO rooms VALUES (?, ?, ?, ?)", office_populator)
+        conn.commit()
+
+        # insert living spaces
+        cursor.executemany("INSERT INTO rooms VALUES (?, ?, ?, ?)", livingspace_populator)
+        conn.commit()
+
+        print "Database populated successfully"
+    except sqlite3.IntegrityError:
+        print "Already populated"
+
+    # for testing
+    return [office_populator, livingspace_populator]
+
+# call the populate function
+populate()
